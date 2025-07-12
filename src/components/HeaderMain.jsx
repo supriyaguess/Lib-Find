88560e296b7cb1214b72f276a724ddd7c2cb5b75
Supriya Kumari <153362616+supriyaguess@users.noreply.github.com>
2025-07-12 20:55:48 +0530

    Update HeaderMain.jsx

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -16,42 +16,42 @@ function HeaderMain({ user, onLogout }) {
           {/* You can refactor these repeated Endpart entries later */}
           <Endpart
             tag="Best Seller"
-            imageAlt="https://www.haverford.edu/sites/default/files/Office/Library/Carrels2.jpg"
-            imageTittle="Luxury Hotel"
-            name="Sunset Resort"
-            location="Goa, India"
-            rating={4.8}
-            reviews={124}
-            description="A luxurious resort with sea view rooms and top-class service."
-            amenities={["Free WiFi", "Pool", "Gym", "Spa"]}
-            price="₹5,499"
-            priceType="per day"
+            imageAlt="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGjafce8IMVXQE6_jfpCTTRYAqH8Xzg-eabQ&s"
+            imageTittle="Central Library Study Desk"
+            name="Central Library Study Desk"
+            location="Delhi University, India"
+            rating={4.9}
+            reviews={320}
+            description="Quiet and focused study spaces with High-speed WiFi abd academic resources."
+            amenities={["Free WiFi", "AC Rooms", "Group Study", "Reading Room"]}
+            price="₹99"
+            priceType="per hour"
           />
           <Endpart
             tag="Best Seller"
-            imageAlt="https://www.haverford.edu/sites/default/files/Office/Library/Carrels2.jpg"
-            imageTittle="Luxury Hotel"
-            name="Sunset Resort"
-            location="Goa, India"
+            imageAlt="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrxlGZlivrqoPXBntciBt9TJRT38gVYrGRH619EHu768v1hap6lw5EmHE8wVNvjBniGq8&usqp=CAU"
+            imageTittle="South Campus"
+            name="South Campus Library"
+            location="South Campus, DU, India"
             rating={4.8}
             reviews={124}
             description="A luxurious resort with sea view rooms and top-class service."
-            amenities={["Free WiFi", "Pool", "Gym", "Spa"]}
-            price="₹5,499"
-            priceType="per day"
+            amenities={["Free WiFi", "AC Rooms", "Group Study", "Silent Zone"]}
+            price="₹79"
+            priceType="per hour"
           />
           <Endpart
             tag="Best Seller"
-            imageAlt="https://www.haverford.edu/sites/default/files/Office/Library/Carrels2.jpg"
-            imageTittle="Luxury Hotel"
-            name="Sunset Resort"
-            location="Goa, India"
-            rating={4.8}
+            imageTittle="North Campus Reading Room"
+            imageAlt="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE_oRhn_faaM0SR1qFvPFPHAaoaz_vU2KWTjLQJ5VICeXhUN2dahswiNlC2HYv0vgKoqU&usqp=CAU"
+            name="North Campus Reading Room"
+            location="North Campus, DU"
+            rating={4.7}
             reviews={124}
-            description="A luxurious resort with sea view rooms and top-class service."
-            amenities={["Free WiFi", "Pool", "Gym", "Spa"]}
-            price="₹5,499"
-            priceType="per day"
+            description="Spacious study cuicles,silent zone, and group discussion rooms with wifi access."
+            amenities={["Free WiFi", "AC", "Silent Zone", "Group Rooms"]}
+            price="₹80"
+            priceType="per hour"
           />
         </div>
         <Smapt />
